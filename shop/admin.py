c453468c8297e8662d7fa314315fde84fe04e903
Evgeniy_Borovoy <borovoyevgen@gmail.com>
2025-08-07 18:57:08 +0400

    add quantity field in admin panel in categories

--- shop/admin.py
+++ shop/admin.py
@@ -11,16 +11,24 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("pk", "title")
+    list_display = ("pk", "title", "get_products_count")
     list_display_links = ("pk", "title")
     prepopulated_fields = {"slug": ("title",)}
+
+    def get_products_count(self, obj):
+        if obj.products:
+            return str(len(obj.products.all()))
+        else:
+            return "0"
+
+    get_products_count.short_description = "Quantity"
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "price", "quantity", "category", "size", "color")
     list_display_links = ("pk", "title")
-    list_editable = ("price", "quantity", "color", "size")
+    list_editable = ("price", "quantity")
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("title", "price")
     inlines = (GalleryInline,)
